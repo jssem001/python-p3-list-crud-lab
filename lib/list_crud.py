1349bc169128d9dcc7db52cc23316eadea83f669
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-27 12:05:47 +0300

    lists

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,41 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    mylist = create_an_empty_list()
+    mylist.append(1)
+    mylist.append("2 fish")
+    mylist.append([3, "cows"])
+    mylist.append("4 cats")
+    return mylist
 
 def add_element_to_end_of_list(l, element):
-    return None
+    # l = create_a_list()
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    # l=create_a_list()
+    l.insert(0,element)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    # l= create_a_list()
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    del l[0]
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+    x=l[0]
+    return x
 
 def retrieve_element_from_index(l, index):
-    return None
+    x=l[index]
+    return x
 
 def retrieve_last_element_from_list(l):
-    return None
+    x=l[-1]
+    return x
